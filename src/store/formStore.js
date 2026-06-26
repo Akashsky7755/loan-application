70110fb4e32fe20ea3717913c8c37d174a6f260a
Akash gautam <cfact6507@gmail.com>
2026-06-27 01:28:45 +0530

    feat(wizard): add Wizard component and Zustand store

---
+++ src/store/formStore.js
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+
+const useFormStore = create((set) => ({
+  currentStep: 1,
+  formData: {
+    step1: {},
+    step2: {},
+    step3: {},
+    step4: {},
+    step5: {},
+    step6: {},
+    step7: {},
+    step8: {},
+  },
+
+  setCurrentStep: (step) => set({ currentStep: step }),
+
+  updateStepData: (step, data) =>
+    set((state) => ({
+      formData: {
+        ...state.formData,
+        [step]: { ...state.formData[step], ...data },
+      },
+    })),
+
+  resetForm: () =>
+    set({
+      currentStep: 1,
+      formData: {
+        step1: {}, step2: {}, step3: {},
+        step4: {}, step5: {}, step6: {},
+        step7: {}, step8: {},
+      },
+    }),
+}));
+
+export default useFormStore;
