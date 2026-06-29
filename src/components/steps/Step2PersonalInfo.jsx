44cbc6e7132d9720b38b6d51ac42f3a8e7784ac9
Akash gautam <cfact6507@gmail.com>
2026-06-30 01:15:18 +0530

    feat(step2): add submitRef and improve personal info validation

--- src/components/steps/Step2PersonalInfo.jsx
+++ src/components/steps/Step2PersonalInfo.jsx
@@ -1,5 +1,6 @@
 import { useForm } from 'react-hook-form';
 import useFormStore from '../../store/formStore';
+import { useForm } from 'react-hook-form';
 
 const Step2PersonalInfo = ({ submitRef }) => {
   const { formData, updateStepData } = useFormStore();
